a5137c3e71865e6ad83ee9561122ceb940226ff0
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2020-04-15 23:18:13 +0200

    organizing redux

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
 import React from 'react';
-import { 
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
- } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 
- import HomePage from './components/HomePage';
- import CreatePage from './components/CreatePage';
- import EditPage from './components/EditPage';
- import NoMatchPage from './components/NoMatchPage';
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <h1>Költségvető</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Főoldal</Link>
-            </li>
-            <li>
-              <Link to="/create">Létrehozás</Link>
-            </li>
-          </ul>
-        </nav>
+  const store = configureStore();
 
-        <Switch>
-          <Route path="/create">
-            <CreatePage />
-          </Route>
-          <Route path="/edit/:id" component={EditPage} />
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route path="*">
-            <NoMatchPage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+  store.dispatch(addExpense({ description: 'Vízdíj' }));
+  console.log(store.getState());
+  return (
+    <AppRouter />
   );
 }
 
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+ } from 'react-router-dom';
+
+ import HomePage from '../components/HomePage';
+ import CreatePage from '../components/CreatePage';
+ import EditPage from '../components/EditPage';
+ import NoMatchPage from '../components/NoMatchPage';
+
+
+const AppRouter = () => (
+  <Router>
+      <div>
+        <h1>Költségvető</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Főoldal</Link>
+            </li>
+            <li>
+              <Link to="/create">Létrehozás</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/create">
+            <CreatePage />
+          </Route>
+          <Route path="/edit/:id" component={EditPage} />
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="*">
+            <NoMatchPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+);
+
+export default AppRouter;
